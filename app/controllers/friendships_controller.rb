d8b5766dce43e85a4d48c3ded2f44383a4910f7b
JackCurtis <jack.v.curtis@gmail.com>
2015-01-09 15:51:58 -0500

    many views and friendship controller

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,16 @@
+class FriendshipsController < ApplicationController
+  def create
+    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
+    if @friendship.save
+      redirect_to root_url
+    else
+
+    end
+  end
+
+  def destroy
+    @friendship = current_user.friendships.find(params[:id])
+    @friendship.destroy
+    redirect_to root_url
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'users#index'
-  resources :users, only: [:show, :index]
+  resources :users, only: [:show, :index] do 
+    resources :friendships, only: [:create, :destroy]
+  end
 
   resources :jokes do
   	resources :comments, except: [:index, :show, :new]
